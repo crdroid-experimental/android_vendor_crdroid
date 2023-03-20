75cb5288d4311a9e982ce22982cc0ca5d0048ab4
Tim Zimmermann <tim@linux4.de>
2023-03-20 20:23:41 +0100

    Initial adblock setup
    Change-Id: I9574f5f56ba58ba604c9da84e56080a6e528832b

--- config/common.mk
+++ config/common.mk
@@ -9,6 +9,13 @@ PRODUCT_BRAND ?= crDroidAndroid
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+# Adblock
+PRODUCT_PACKAGES += \
+    hosts.adblock
+
+PRODUCT_COPY_FILES += \
+    vendor/lineage/etc/init/init.adblock.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.adblock.rc
+
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google
